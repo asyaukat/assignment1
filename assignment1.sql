0ae7e9aa6d47c6bd294557d0a9b9051e40b68e65
asyaukat <102497212+asyaukat@users.noreply.github.com>
2022-03-28 11:49:37 +0800

    Add files via upload

---
+++ assignment1.sql
@@ -0,0 +1,10 @@
+CREATE table STUDENT (
+studentID  int ,
+studentName varchar(45),
+studentDept varchar(45),
+CONSTRAINT student_PK PRIMARY KEY (studentID)
+);
+
+INSERT INTO STUDENT VALUES('1','Ahmad','Network');
+INSERT INTO STUDENT VALUES('2','Hafiz','Software Engineering');
+INSERT INTO STUDENT VALUES('3','Lisa','Multimedia');
